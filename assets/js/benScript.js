9375d262efe87aeca4c8481891d8a007bde8e7d0
Ben Gianni <bgianni576@gmail.com>
2019-07-20 17:01:48 -0400

    got rid of footer

--- assets/js/benScript.js
+++ assets/js/benScript.js
@@ -1,5 +1,5 @@
 function Attention() {
-	alert("starting at client");
+	console.log("starting at client");
 	var send = new XMLHttpRequest();
 	send.open("GET", "assets/php/test.php");
 	send.send();
@@ -7,7 +7,7 @@ function Attention() {
 	send.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
 			var obj = JSON.parse(this.responseText);
-			alert('response from server: ' + obj.name + ' is ' + obj.age);
+			console.log('response from server: ' + obj.name + ' is ' + obj.age);
 			document.getElementById('test_button').innerHTML = obj.name + ' is ' + obj.age;
 		}
 	};
